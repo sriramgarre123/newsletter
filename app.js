da35b5ac4b5f8ffeab4697d8b9bcd2bbc174014f
sriramgarre123 <58555387+sriramgarre123@users.noreply.github.com>
2019-12-10 09:44:12 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,64 +1,60 @@
-const express = require("express");
-const bodyParser= require("body-parser");
-const app = express();
-const request = require("request");
-app.use(bodyParser.urlencoded({extended: true}));
+const express = require( 'express');
+const bodyparser=require("body-parser");
+const request= require('request');
 
-app.use(express.static("public"))
+app=express();
+
+app.use(express.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname +"/signup.html");
+    res.sendFile( __dirname+"/signup.html");
 });
+
 app.post("/",function(req,res){
-    var fname =req.body.firstname;
-    var lname =req.body.lastname;
-    var email =req.body.email;
-    var data ={
-        members: [
+    var email=req.body.email;
+    var fname=req.body.fname;
+    var contact=req.body.contact;
+
+    console.log(email,fname,contact);
+
+    var data={
+        members:[
             {
-                
-                email_address: email,
-                status: "subscribed",
-                merge_fields:{
-                    FNAME: fname,
-                    LNAME: lname,
-
-
+                email_address:email,
+                status:"subscribed",
+                "merge_fields":{
+                    FNAME:fname,
+                    PHONE:contact
                 }
             }
         ]
     };
 
-    var jsonData =JSON.stringify(data);
-  var options={
-    url:"https://us4.api.mailchimp.com/3.0/lists/178cf9c268",
-    method : "POST",
-    headers:{
-        "Authorization":"sriram 16e1591fa9c5f8862cf0494ea53f20b9-us4"
-    },
-    body : jsonData
-    
-  };
-    
-    request(options, function(error,response,body){
-      if(error){
-          res.send("Go and try Again")
-        //   console.log(error)
-              }
-   else{
-       res.sendFile(__dirname + "/sucess.html");
-       console.log(response.statusCode)
-   }
-            })
+    var jasondata=JSON.stringify(data);
 
+    var option={
+        url:"https://us4.api.mailchimp.com/3.0/lists/178cf9c268",
+        method:"POST",
+        headers:{
+            "Authorization":"anshumanchaurasia 9189b4b0277c06ffb05a465f72a7967d-us4"
+        },
+        body:jasondata
+    };
+
+    request(option,function(error,response,body){
+        if(error)
+            console.log("error in req if");
+        else{
+            console.log("success"+response.statusCode);
+            res.send("success");
+        }
     });
-
-app.listen(process.env.PORT || 3000 ,function()
-{
-console.log("running successfully")
-})
+});
 
 
 
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Server has started");
+});
 
 
